b3a304ff8562dbead4cb360bc4e94905ea40183a
Hideki Yoshikura <evenintherain@gmail.com>
2021-10-16 22:19:36 +0900

    疎通確認用

---
+++ src/sample-deploy-dotnet5/sample-deploy-dotnet5/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "sampleresponse";
+
+        }
+    }
+}
